47df6a13eb15e9b7158f3276b0a11734c5ace0ee
Misha Shmidt <mishashmidt0@gmail.com>
2022-05-24 02:12:51 +0300

    feat: add task4

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,27 @@
+const fs = require('fs');
+const path = require('path');
 
+let pathToFiles = path.join(__dirname, 'files');
+let pathToCopy = path.join(__dirname, 'files-copy');
+
+fs.access(pathToCopy, (err) => {
+  if (err) {
+    copyDir(pathToFiles, pathToCopy);
+  } else {
+    fs.rm(path.join(pathToCopy), {recursive: true}, () => {
+      copyDir(pathToFiles, pathToCopy);
+    });
+  }
+});
+
+async function copyDir(oldDir, newDir) {
+  await fs.promises.mkdir(newDir, {recursive: true});
+  const files = await fs.promises.readdir(oldDir, {withFileTypes: true});
+  for (const file of files) {
+    if (file.isFile()) {
+      fs.promises.copyFile(path.join(oldDir, `${file.name}`), path.join(newDir, `${file.name}`));
+    } else {
+      copyDir(path.join(oldDir, `${file.name}`), path.join(newDir, `${file.name}`));
+    }
+  }
+}
